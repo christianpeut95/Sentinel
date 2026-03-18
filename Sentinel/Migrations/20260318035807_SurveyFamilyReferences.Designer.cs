ec4b698580bd43f86640ebd147a4e68e74b05a71
christianpeut95 <christianpeut95@users.noreply.github.com>
2026-03-18 14:31:45 +1030

    Fix SurveyFamilyReferences migration: add Designer.cs so it runs on other servers

--- Sentinel/Migrations/20260318035807_SurveyFamilyReferences.Designer.cs
+++ Sentinel/Migrations/20260318035807_SurveyFamilyReferences.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sentinel.Data;
 
@@ -11,9 +12,11 @@ using Sentinel.Data;
 namespace Sentinel.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260318035807_SurveyFamilyReferences")]
+    partial class SurveyFamilyReferences
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
